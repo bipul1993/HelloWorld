cb9e9debfa52640ad7ff65b5036926823725a35f
bipul1993 <65331070+bipul1993@users.noreply.github.com>
2021-01-27 19:57:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world made changes now please do!!");
+		System.out.println("Hello world made changes now please do web hook!!");
 	}
 
 }
